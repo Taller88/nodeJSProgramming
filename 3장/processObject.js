8cf17a99b719e0dc4644af08aaa3b59ad0d227eb
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2022-01-30 20:29:15 +0900

    4,5 장 initial Commit

--- 3장/processObject.js
+++ 3장/processObject.js
@@ -18,3 +18,13 @@ console.log("process.platform: "+process.platform);
 process.argv.forEach(function(item, index) {
     console.log(index+" : "+ typeof(item)+": "+ item)
 });
+/*
+실행 operation  : node processObject.js 1000 2000
+
+0 : string: C:\Program Files\nodejs\node.exe
+1 : string: C:\Users\user\Desktop\NodeJSProgramming\3장\processObject.js
+2 : string: 1000
+3 : string: 2000
+
+
+*/
