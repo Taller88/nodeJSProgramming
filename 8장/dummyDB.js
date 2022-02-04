1c829dd5376ace4249334145a23a69b624d01ea7
JinwooJung <iod1124@coocon.net>
2022-02-04 18:01:00 +0900

    DB connection 관련 commit

---
+++ 8장/dummyDB.js
@@ -0,0 +1,43 @@
+var DummyDB = (function(){
+    var DummyDB = {};
+    var storage = {};
+    var count = 1;
+
+    DummyDB.get = function(id){
+        if(id){
+            id = (typeof id === 'string')? Number(id): id;
+
+            for(var i in storage){
+                if(storage[i].id == id){
+                    return storage[i];
+                }
+            }
+    
+        }else{
+            return storage;
+        }
+    }
+
+    DummyDB.insert = function(data){
+        data.id = count++;
+
+        storage.push(data);
+        return data;
+    }
+
+    DummyDB.remove = function(id){
+        if(id){
+            id = (typeof id === 'string')?Number(id): id;
+            
+            for(var i in storage){
+                if(storage[i].id === id){
+                    storage.splice(i,1);
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+    return DummyDB;
+})();
